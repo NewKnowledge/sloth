e320533fbb34a26dc35aec59d8edb64149b5e83a
azunre <azunre@PaulLinuxGPUDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2018-06-22 14:33:08 +0000

    minor change to make visualization clearer

--- Sloth/Sloth.py
+++ Sloth/Sloth.py
@@ -18,11 +18,11 @@ class Sloth:
         ax1 = fig.add_subplot(211)
         ax1.plot(np.arange(series.shape[1]-1), series.values[i1,1:])
         ax1.set_xlabel("time")
-        ax1.set_ylabel("i1")
+        ax1.set_ylabel(str(i1)
         ax2 = fig.add_subplot(212)
         ax2.plot(np.arange(series.shape[1]-1), series.values[i2,1:])
         ax2.set_xlabel("time")
-        ax2.set_ylabel("i2")
+        ax2.set_ylabel(str(i2))
         plt.show()
 
     def GenerateSimilarityMatrix(self,series):
