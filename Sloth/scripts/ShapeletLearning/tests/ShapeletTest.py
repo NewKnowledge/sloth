3e94caf34ae6bf7fa139008a1e6d0e9bc8b32081
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2018-10-31 21:15:22 +0000

    add local test of shapelet class

---
+++ Sloth/scripts/ShapeletLearning/tests/ShapeletTest.py
@@ -0,0 +1,19 @@
+from Sloth import Shapelets
+from tslearn.datasets import CachedDatasets
+from sklearn.metrics import accuracy_score
+
+# constants
+epochs = 200
+shapelet_length = 0.1
+num_shapelet_lengths = 2
+time_series_id = 0
+
+# create shapelets
+X_train, y_train, X_test, y_test = CachedDatasets().load_dataset("Trace")
+trace_shapelets = Shapelets(X_train, y_train, epochs, shapelet_length, num_shapelet_lengths)
+
+# test methods
+predictions = trace_shapelets.PredictClasses(X_test)
+print("Accuracy = ", accuracy_score(y_test, predictions))
+trace_shapelets.VisualizeShapelets()
+trace_shapelets.VisualizeShapeletLocations(X_test, time_series_id)
