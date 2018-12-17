c94ccbb1f709c6d384ca42bb440d8cce2bc23a3d
Ubuntu <jgleason@JeffreyVMDev.umi1swgzx55e3jlvq0v5igys1h.jx.internal.cloudapp.net>
2018-12-17 22:12:02 +0000

    add boxcox transformation function and ARIMA testing on SEISMIC data notebook

--- Sloth/Sloth.py
+++ Sloth/Sloth.py
@@ -4,7 +4,7 @@ import pandas as pd
 #matplotlib.use('Agg') # uncomment for docker images
 import matplotlib.pyplot as plt
 import pickle
-from scipy import sparse
+from scipy import sparse, stats
 import hdbscan
 from sklearn.cluster import DBSCAN
 from scipy.spatial.distance import euclidean
@@ -138,6 +138,14 @@ class Sloth:
 
     def ScaleSeriesMinMax(self, series, min, max):
         return TimeSeriesScalerMinMax(min = min, max = max).fit_transform(series)
+
+    def ScaleSeriesBoxCox(self, series):
+        min_value = min(series)
+        if min_value < 0:
+        # scale series to positive values only
+            series = [i + abs(min_value) + 1 for i in series]
+        transformed, lam = stats.boxcox(series)
+        return transformed
 
     def FitSeriesARIMA(self, data, seasonal, *seasonal_differencing):
         # default: annual data
