c8d677e81314f40deef34ab6a8e5df6ee330df6f
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-02-05 15:13:15 -0600

    downgrade version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name='Sloth',
-    version='2.0.3',
+    version='2.0.2',
     description='Time series tools for classification, forecasting and clustering',
     packages=['Sloth'],
     install_requires=['scikit-learn >= 0.18.1',
