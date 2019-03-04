56d8e05ded266168281b9ccd22dcf8c6e04e722b
jgleason <jeffrey.gleason@newknowledge.io>
2019-03-04 01:29:37 +0000

    condense events_to_rates method, add param

--- Sloth/preprocess.py
+++ Sloth/preprocess.py
@@ -3,46 +3,30 @@ from scipy.ndimage.filters import gaussian_filter1d
 import numpy as np
 from sklearn.kernel_approximation import RBFSampler
 
-def _events_to_rates_helper(event_times, num_bins=72, max_time=None):
-    """ convert lists of event times into rate functions with a discrete time bin_size of 1/rates_per_unit.
+def events_to_rates(event_times, filter_bandwidth=1, num_bins=72, min_time = None,max_time=None, density = True):
+    """ convert list of event times into rate function with a discrete time bin_size of 1/rates_per_unit.
     Uses a guassian filter over the empirical rate (histogram count / bin_size) """
 
     if len(event_times) == 0:  # if event times is an empty list or array
         print("empty event_times list/array")
         return np.zeros(num_bins), np.zeros(num_bins)
 
-    # if its a nonempty list of floats, wrap in a list
-    if not isinstance(event_times[0], Iterable):
-        event_times = [event_times]
-
     if not max_time:
-        max_time = max(max(times) for times in event_times)
-    min_time = min(min(times) for times in event_times)
+        max_time = max(event_times)
+    if not min_time:
+        min_time = min(event_times)
     bins = np.linspace(min_time, max_time, num=num_bins + 1)
     rate_times = (bins[1:] + bins[:-1]) / 2
 
     bin_size = (max_time - min_time) / num_bins
-
-    return rate_times, bins, bin_size
-
-def events_to_rates(event_times, filter_bandwidth=3, num_bins=72, max_time=None):
-    """ convert lists of event times into rate functions with a discrete time bin_size of 1/rates_per_unit.
-    Uses a guassian filter over the empirical rate (histogram count / bin_size) """
-
-    rate_times, bins, bin_size = _events_to_rates_helper(event_times, num_bins, max_time)
-    counts = np.array([np.histogram(times, bins=bins)[0] for times in event_times])
-    sampled_rates = counts / bin_size
+    if density:
+        counts = np.array(np.histogram(event_times, bins=bins)[0])
+        sampled_rates = counts / sum(counts)
+    else:
+        counts = np.array(np.histogram(event_times, bins=bins)[0])
+        sampled_rates = counts / bin_size
     rate_vals = gaussian_filter1d(sampled_rates, filter_bandwidth, mode="nearest")
     return rate_vals, rate_times
-
-def events_to_densities(event_times, filter_bandwidth=3, num_bins=72, max_time=None):
-    """ convert lists of event times into densities with a discrete time bin_size of 1/rates_per_unit.
-    Uses a guassian filter over the empirical rate (histogram count / bin_size) """
-
-    rate_times, bins, _ = _events_to_rates_helper(event_times, num_bins, max_time)
-    densities = np.array([np.histogram(times, bins=bins, density=True)[0] for times in event_times])
-    rate_vals = gaussian_filter1d(densities, filter_bandwidth, mode="nearest")
-    return rate_vals, rate_times    
 
 def rand_fourier_features(rate_vals, dim=1000, random_state=0):
     if rate_vals.ndim == 1:
