db37e131cdff65ad83eed9dd03fcac6dab9e4538
amably <41767329+amably@users.noreply.github.com>
2019-11-19 11:24:02 -0600

    Empty init

--- Sloth/__init__.py
+++ Sloth/__init__.py
@@ -1,13 +1,1 @@
-from Sloth.cluster import KMeans
-from Sloth.classify import Shapelets
-from Sloth.classify import Knn
-from Sloth.predict import Arima
-
-__version__ = '2.0.7'
-
-__all__ = [ 'KMeans',
-            'Shapelets',
-            'Knn',
-            'Arima',
-            ]
 
